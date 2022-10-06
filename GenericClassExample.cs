f30ba437d658f7dd1d4f6b4371a8dcc2d8d87233
Akanksha <akanksha.shrivastava@goavega.com>
2022-10-06 16:47:19 +0530

    06-10-2022

---
+++ GenericClassExample.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Generics
+{
+    class GenericClass<T>
+    {
+      public GenericClass(T msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+
+    class Genericxamples
+    {
+        public static void Main(string[] args)
+        {
+            GenericClass<string> genC = new GenericClass<string>("This is Generic class");
+            GenericClass<int> genCi = new GenericClass<int>(23);
+            GenericClass<char> genCh = new GenericClass<char>('A');
+            GenericMethodExample example = new GenericMethodExample();
+            example.GenericMethod("This is Generic Method");
+            example.GenericMethod(123);
+            example.GenericMethod('S');
+            var stack = new SimpleStack<double>();
+            stack.Push(1.7);
+            stack.Push(2.3);
+            stack.Push(3.0);
+            double sum = 0.0;
+            while (stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+            Console.WriteLine($"Sum is: {sum}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ GenericMethodExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class GenericMethodExample
+    {
+        public void GenericMethod<T>(T msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
