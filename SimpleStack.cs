66f3382301098c71649c17ba8ba03639f815adac
Akanksha <akanksha.shrivastava@goavega.com>
2022-10-06 16:49:12 +0530

    06-10-2022

---
+++ SimpleStack.cs
@@ -0,0 +1,14 @@
+﻿namespace Generics
+{
+    public class SimpleStack<TypePlaceHolder>
+    {
+        private readonly TypePlaceHolder[] _items;
+        private int _currentIndex = -1;
+        public SimpleStack() => _items = new TypePlaceHolder[10];
+        public int Count => _currentIndex +1;
+        public void Push(TypePlaceHolder item) => _items[++_currentIndex] = item;
+
+        public TypePlaceHolder Pop() => _items[_currentIndex--];
+
+    }
+}
